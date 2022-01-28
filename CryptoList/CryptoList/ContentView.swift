6f40c2735a3cb7156ef42560b796f084d57049dc
Martin Dimitrov <martinreff1@gmail.com>
2022-01-28 17:16:38 +0200

    Add: base project

---
+++ CryptoList/CryptoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoList
+//
+//  Created by Martin Dimitrov on 28.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoList/CryptoList/CryptoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListApp.swift
+//  CryptoList
+//
+//  Created by Martin Dimitrov on 28.01.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
